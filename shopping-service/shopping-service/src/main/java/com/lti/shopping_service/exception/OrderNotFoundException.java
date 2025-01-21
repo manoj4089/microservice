d8de4a9ae6b1c0a02e79450694b23b101381a7b0
manoj4089 <80942224+manoj4089@users.noreply.github.com>
2025-01-21 10:22:34 +0530

    Delete shopping-service directory

--- shopping-service/shopping-service/src/main/java/com/lti/shopping_service/exception/OrderNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.lti.shopping_service.exception;
-
-public class OrderNotFoundException extends RuntimeException{
-    public OrderNotFoundException(String message){
-        super(message);
-    }
-}
--- shopping-service/shopping-service/src/main/java/com/lti/shopping_service/repository/OrderRepo.java
+++
@@ -1,9 +0,0 @@
-package com.lti.shopping_service.repository;
-
-import com.lti.shopping_service.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepo extends JpaRepository<Order, Long> {
-}
